2b09bef1b8fd16585c2b4118ab48b230e0709da3
Viktor Bachmanov <vbachmanov@mail.ru>
2024-06-01 10:28:39 +0300

    Install TailwindCSS

--- src/App.vue
+++ src/App.vue
@@ -7,10 +7,16 @@ import CheckboxVB from './components/CheckboxVB.vue'
 
 const checked_1 = ref(false)
 const checked_2 = ref(false)
+
+function handleToggleTheme() {
+  document.documentElement.classList.toggle('dark')
+}
 </script>
 
 <template>
   <div style="display: flex; flex-direction: column; gap: 1em">
+    <button @click="handleToggleTheme">Toggle theme</button>
+
     <label>
       <CheckboxVB 
         v-model="checked_1"
@@ -21,9 +27,14 @@ const checked_2 = ref(false)
     <label>
       <CheckboxVB 
         v-model="checked_2"
+        class="!text-blue-700 dark:!text-rose-700"
       />
       CheckboxVB-2
     </label>
+
+    <div class="bg-stone-500">
+      TailwindCSS
+    </div>
   </div>
   <!-- <HelloWorld msg="Vite + Vue" /> -->
 </template>
