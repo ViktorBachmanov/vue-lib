960c97fee27bcda48cbfc3b8ea231fb808b7fe5c
Viktor Bachmanov <vbachmanov@mail.ru>
2024-06-13 12:47:23 +0300

    Fix: export the TextareaVB.vue component

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import CheckboxVB from "./components/CheckboxVB.vue";
 import InputVB from "./components/InputVB.vue";
+import TextareaVB from "./components/TextareaVB.vue";
 
-export { CheckboxVB, InputVB };
+export { CheckboxVB, InputVB, TextareaVB };
