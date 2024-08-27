3a8ab0c33a8c2cce5b7ee2277deff11d46a5d789
Viktor Bachmanov <vbachmanov@mail.ru>
2024-08-27 09:02:32 +0300

    select-vb component has been done

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import CheckboxVB from "./components/CheckboxVB.vue";
 import InputVB from "./components/InputVB.vue";
 import TextareaVB from "./components/TextareaVB.vue";
+import SelectVB from "./components/SelectVB.vue";
 
 export { CheckboxVB, InputVB, TextareaVB };
