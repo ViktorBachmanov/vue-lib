938ff344660619c22d218981b1c11d85f6908a97
Viktor Bachmanov <vbachmanov@mail.ru>
2024-12-09 15:42:49 +0300

    continue

--- src/components/DateInputPart.vue
+++ src/components/DateInputPart.vue
@@ -1,26 +1,31 @@
 <script setup>
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
 
 const props = defineProps({
   prefix: String,
 })
 
-const val = ref('01')
+const val = ref(1)
 const valRef = ref(null)
+
+const cVal = computed({
+  get() {
+    return Number(val.value) > 9 
+      ? val.value
+      : '0' + val.value
+  },
+  set: newVal => val.value = newVal 
+})
 
 const focused = ref(false)
 
+const digitStrokesCount = ref(0)
+
 function handleFocus() {
-  // const range = new Range()
-  // range.setStart(valRef.value, 0)
-  // range.setEnd(valRef.value, 1)
-
-  // document.getSelection().removeAllRanges();
-  // document.getSelection().addRange(range);
-
   const event = new Event('focusDateInputVBPart')
   window.dispatchEvent(event)
   addEventListener('focusDateInputVBPart', handleBlur)
+  addEventListener('keydown', handleKeydown)
 
   focused.value = true
 }
@@ -28,6 +33,24 @@ function handleFocus() {
 function handleBlur() {
   focused.value = false
   removeEventListener('focusDateInputVBPart', handleBlur)
+  removeEventListener('keydown', handleKeydown)
+  digitStrokesCount.value = 0
+}
+
+function handleKeydown(e) {
+  if (e.key >= '0' && e.key <= '9') { 
+    switch (digitStrokesCount.value++) {
+      case 0:
+        val.value = e.key
+        break
+      case 1:
+        val.value += e.key
+        break
+      default:
+        handleBlur()
+        break
+    }
+  }
 }
 </script>
 
@@ -36,7 +59,7 @@ function handleBlur() {
     style="display: inline; cursor: pointer;"
     @click="handleFocus"
   >
-    <span v-if="prefix">{{ prefix }}</span><span ref="valRef" :class="{ focused: focused }">{{ val }}</span>
+    <span v-if="prefix">{{ prefix }}</span><span ref="valRef" :class="{ focused: focused }">{{ cVal }}</span>
   </div>
 </template>
 
--- src/components/DateInputVB.vue
+++ src/components/DateInputVB.vue
@@ -1,10 +1,21 @@
 <script setup>
 import DateInputPart from './DateInputPart.vue';
+import DateInputDigit from './DateInputDigit.vue';
 // const date = ref((new Date).toDateString())
+
+// const currentDigitIndex = ref(null) // 1...6
 </script>
 
 <template>
   <div>
-    <DateInputPart />.<DateInputPart />.<DateInputPart prefix="20" />
+    <DateInputPart 
+    />.<DateInputPart 
+    />.<DateInputPart 
+      prefix="20" 
+    />
+
+    <div class="mt-3">
+      <DateInputDigit />
+    </div>
   </div>
 </template>
