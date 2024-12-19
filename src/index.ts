247f798ab1058ca75d62e528e6e139364a07db1e
Viktor Bachmanov <vbachmanov@mail.ru>
2024-12-19 15:56:23 +0300

    fix typo

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,6 @@ import CheckboxVB from "./components/CheckboxVB.vue";
 import InputVB from "./components/InputVB.vue";
 import TextareaVB from "./components/TextareaVB.vue";
 import SelectVB from "./components/SelectVB.vue";
-import DateInputVB from "./components/SelectVB.vue";
+import DateInputVB from "./components/DateInputVB.vue";
 
 export { CheckboxVB, InputVB, TextareaVB, SelectVB, DateInputVB };
